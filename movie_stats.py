2c4e2fc09c1f595661e81344056ceede665beb63
Ryan S. Smith <ryans.smith@veteransunited.com>
2024-06-10 13:48:38 -0500

    Modified movie_stats to generate figure

--- movie_stats.py
+++ movie_stats.py
@@ -1,4 +1,4 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 from movie_rolls import movie_night
 
@@ -10,23 +10,30 @@ def run_simulation(n_movies, n_sims=1_000):
     for _ in range(n_sims):
         rolls_needed = movie_night(n_movies, show=False)
         results.append(rolls_needed)
-    return results
-
-# def plot_results(n_movies, results):
-#     """Plot the distribution of the number of rolls needed."""
-#     plt.hist(results, bins=30, edgecolor='black')
-#     plt.title(f"Distribution of Rolls Needed for {n_movies} Movies")
-#     plt.xlabel("Number of Rolls")
-#     plt.ylabel("Frequency")
-#     plt.show()
+    return np.array(results)
 
 ####################################################################################################
 
 if __name__ == "__main__":
+    ## prepare to run Monte-Carlo simulations and plot the results
     movie_counts = np.arange(2, 16)
     n_sims = 1_000
+    fig, axs = plt.subplots(figsize=(5,7), nrows=2)
 
+    ## run the sims
     for n_movies in movie_counts:
         results = run_simulation(n_movies, n_sims)
-        # plot_results(n_movies, results)
-        print(f"{n_movies} movies --> {np.mean(results):.2f} rolls needed on average")
+        print(f"{n_movies} movies --> {results.mean():.2f} rolls needed on average")
+        axs[0].plot(n_movies, results.mean(), 'o')
+        axs[1].hist(results, density=False, bins=20, ec='k', alpha=0.5,) # label=f"{n_movies} movies")
+
+    ## customize and save the figure
+    axs[0].set_xlabel("# of movies")
+    axs[0].set_ylabel("Mean # of rolls")
+    axs[0].set_xticks(np.arange(2, 18, 2))
+    
+    axs[1].set_xlabel("# of rolls")
+    axs[1].set_ylabel("Count")
+
+    plt.tight_layout()
+    plt.savefig("rolls-count.pdf")
